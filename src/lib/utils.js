b95aaaafdc3d2b3787dbc0996e035def80f5f958
Prakash Jagtap <prakashjagtap738760@gmail.com>
2024-08-01 12:59:13 +0530

    update mongodb uri

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,7 +8,7 @@ export const connectToDb = async () => {
       console.log("Using existing connection");
       return;
     }
-    const db = await mongoose.connect(process.env.MONGO);
+    const db = await mongoose.connect("mongodb://localhost:27017");
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
     console.log(error);
